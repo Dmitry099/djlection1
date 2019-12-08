dc2e4b2a349201c74f43c38995fb8157e90b1911
Dmitry Lomaev <lomaevd@gmail.com>
2019-12-08 20:59:10 +0400

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+import os
+
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+    
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.now().time()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    project_path = os.listdir(path=".")
+    msg = f"Cписок файлов и директорий в папке: {', '.join( project_path)}"
+    return HttpResponse(msg)
